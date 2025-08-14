775417582e2c49eaa493a9192c4348e1b8df2fd4
Andy <charlotte131498349@gmail.com>
2025-08-14 14:12:24 -0400

    move call_api.py

--- persona_generator/call_api.py
+++ persona_generator/call_api.py
@@ -50,8 +50,8 @@ def call_api(input_file, output_file):
 
 def main() -> None:
     
-    inputs_folder = Path("./persona_generator/testdata/inputs")
-    outputs_folder = Path("./persona_generator/testdata/outputs")
+    inputs_folder = Path("./testdata/inputs")
+    outputs_folder = Path("./testdata/outputs")
     
     # Find input file using prefix matching for double-digit numbers
     for i in range(INPUT_CODE_START, INPUT_CODE_END+1):
