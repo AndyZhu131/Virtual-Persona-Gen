65a25bd0af904474bb84acc569326c13184ae28c
Andy <charlotte131498349@gmail.com>
2025-08-20 22:55:10 -0400

    storybook added and gitignore updated

--- vpg-ui/.eslintrc.cjs
+++ vpg-ui/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 module.exports = {
     root: true,
-    extends: ["next/core-web-vitals"],
+    extends: ["next/core-web-vitals", "plugin:storybook/recommended"],
     rules: { "@next/next/no-img-element": "off" }
   };
   
